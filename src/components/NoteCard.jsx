e293a3552051e501626d101bbd879d0692b897c3
jello-2 <jellosmcacc1@outlook.com>
2024-09-29 11:58:02 -0700

    Sample notecard is ready
    Sample notecard works. Let's refactor for everything and make a template

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import {setNewOffset, setZIndex} from '../utils.js'
 
 const NoteCard = ({ note }) => {
-    let position = JSON.parse(note.position);
+    const [position, setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
     const textAreaRef = useRef(null);
+
+    let mouseStartPos = { x: 0, y: 0 };
+ 
+    const cardRef = useRef(null);
+
 
     useEffect(() => {
         autoGrow(textAreaRef);
@@ -15,6 +21,38 @@ const NoteCard = ({ note }) => {
         current.style.height = "auto"; // Reset the height
         current.style.height = current.scrollHeight + "px"; // Set the new height
     }
+
+    const mouseDown = (e) => {
+        setZIndex(cardRef.current);
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+     
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
+
+
+
+    const mouseMove = (e) => {
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+     
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+     
+        const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+        setPosition(newPosition);
+     
+    };
+
+
     return (
         <div
         className="card"
@@ -24,8 +62,19 @@ const NoteCard = ({ note }) => {
             top: `${position.y}px`,
         }}
     >
+        <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown = { mouseDown }
+    ></div>
+
+        
              <div className="card-body">
-        <textarea
+             <textarea
+            onFocus={() => {
+                setZIndex(cardRef.current);
+}}
+
     ref={textAreaRef}
             style={{ color: colors.colorText }}
             defaultValue={body}
