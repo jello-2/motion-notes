68d2d139f003468e7093cdfe97d5b8774348ab74
AStrangePotato <danielzhang.936@gmail.com>
2024-09-29 17:05:02 -0700

    some final clenaup

--- src/room/CreateRoom.jsx
+++ src/room/CreateRoom.jsx
@@ -1,10 +1,9 @@
-import { collection, doc, setDoc } from "firebase/firestore";
-import { db } from "../Firebase";
+import addWidget from "./AddWidget";
 
 function generateRoomCode() {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let roomCode = "";
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
         const randomIndex = Math.floor(Math.random() * letters.length);
         roomCode += letters[randomIndex];
     }
@@ -14,27 +13,10 @@ function generateRoomCode() {
 const createRoom = async () => {
     const roomCode = generateRoomCode();
     try {
-        const roomRef = doc(collection(db, roomCode));
-
-        // Add the data to the new collection
-        await setDoc(roomRef, {
-                type: 'note',
-                body: JSON.stringify(
-                  'New note...'
-                ),
-                colors: JSON.stringify({
-                  id: "color-purple",
-                  colorHeader: "#FED0FD",
-                  colorBody: "#FEE5FD",
-                  colorText: "#18181A",
-                }),
-                position: JSON.stringify({ x: 505, y: 10 }),
-                width: "400",
-        });
-
+        addWidget(roomCode, 'note');
         console.log(`Room created with collection: ${roomCode}`);
-
         window.location.href = `/room/${roomCode}`;
+        
     } catch (e) {
         console.error("Error creating room: ", e);
     }
