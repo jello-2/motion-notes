4d3d7d1d16bf4999d041adf306a71ce11e803f6f
AStrangePotato <danielzhang.936@gmail.com>
2024-09-30 20:41:29 -0700

    some local background preset changes
    so we dont blow out firebase

--- src/room/Room.jsx
+++ src/room/Room.jsx
@@ -6,7 +6,7 @@ import fetchRoomData from './FetchRoom';
 import addWidget from './AddWidget';
 import ShareRoom from './ShareRoom';
 import SettingsModal from './Settings';
-import { Share2, Tag, Clock, Music, PenTool, Settings, ChevronRight, ChevronLeft, PlusSquare, HelpCircle } from 'react-feather';
+import { Share2, Clock, Music, PenTool, Settings, PlusSquare, HelpCircle } from 'react-feather';
 import { collection, onSnapshot, doc, getDoc, setDoc } from 'firebase/firestore';
 import { db } from '../Firebase';
 
@@ -26,7 +26,7 @@ const SidebarButton = ({ icon: Icon, onClick, label }) => {
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <Icon size={25} color='black'/>
+          <Icon size={24} color='black'/>
         </button>
         {isHovered && (
           <div className="absolute left-12 top-1/2 -translate-y-1/2 bg-white px-2 py-1 rounded shadow-md text-sm whitespace-nowrap text-black">
@@ -80,8 +80,6 @@ const Room = () => {
         { icon: Share2, onClick: () => setShareOpen(prev => !prev), label: "Share Room" },
     ];
 
-
-
     const handleDelete = (widgetId) => {
         setRoomData(prevData => prevData.filter(widget => widget.id !== widgetId));
     };
@@ -97,7 +95,6 @@ const Room = () => {
     };
     
     return (
-        
         <div>
             <ShareRoom 
                 isOpen={isShareOpen} 
@@ -122,7 +119,8 @@ const Room = () => {
                     backgroundPosition: 'center',
                 }}
             >
-{/* Overlay div for darkness */}
+
+            {/* Overlay div for darkness */}
             <div
                 className="fixed inset-0 pointer-events-none"
                 style={{
@@ -149,17 +147,14 @@ const Room = () => {
                 </div>
             </div>
 
-{/* Toolbar */}
-
-
- <div className="fixed left-2 top-1/2 -translate-y-1/2 z-50 flex flex-col items-center bg-white p-2 rounded-full shadow-lg">
+            {/* Toolbar */}
+            <div className="fixed left-2 top-1/2 -translate-y-1/2 z-50 flex flex-col items-center bg-white p-2 rounded-full shadow-lg">
                 <div className="flex flex-col items-center space-y-4 py-2">
                     {buttons.map((button, index) => (
                         <SidebarButton key={index} {...button} />
                     ))}
                 </div>
             </div>
-
         </div>
     );
 };
